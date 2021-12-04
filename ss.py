aa85ac579a074231835d44b31db5c7d0361015bf
Ben Combs <thecombs.ben@gmail.com>
2021-12-04 20:15:20 +0000

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,12 @@
+###############################################
+# Group Name  : XXXXXX
+
+# Member1 Name: XXXXXX
+# Member1 SIS ID: XXXXXX
+# Member1 Login ID: XXXXXX
+
+# Member2 Name: XXXXXX
+# Member2 SIS ID: XXXXXX
+# Member2 Login ID: XXXXXX
+###############################################
+
