6e5bf7b730bc60abbc3f0f50f66760c2d450b997
Ben Combs <thecombs.ben@gmail.com>
2021-12-06 18:00:00 +0000

    Add files via upload

--- awget.py
+++ awget.py
@@ -1,12 +1,82 @@
 ###############################################
-# Group Name  : XXXXXX
+# Group Name  : SAP
 
-# Member1 Name: XXXXXX
-# Member1 SIS ID: XXXXXX
-# Member1 Login ID: XXXXXX
+# Member1 Name: Ben Combs
+# Member1 SIS ID: 831850566
+# Member1 Login ID: bcombs18
 
 # Member2 Name: XXXXXX
 # Member2 SIS ID: XXXXXX
 # Member2 Login ID: XXXXXX
 ###############################################
 
+import getopt
+import socket
+import sys
+import argparse
+import random
+
+def formatMessage(url, chain):
+    message = url + "\n"
+    for ss in chain :
+        message = message + '{} {}\n'.format(chain[0], chain[1])
+    return message
+
+def parseChain(chainStr):
+    chain = []
+    for line in chainStr.splitlines():
+        line = line.strip()
+        if line:
+            parsed = line.split()
+            ip = parsed[0]
+            port = int(parsed[1])
+            chain.append((ip, port))
+    return chain
+
+def parseChainfile(chainfile):
+    with open(chainfile, 'r') as file:
+        #First line of chain file not needed so skip it
+        file.readline()
+        data = file.read()
+    
+    chain = parseChain(data)
+    print("chainlist is")
+    for ss in chain:
+        print("{}".format(ss))
+    return chain
+
+def run_awget(args):
+    print("awget:")
+    url = args.url
+    print("Request: {}".format(url))
+    chainfile = args.chainfile
+    chain = parseChainfile(chainfile)
+    nextIP, nextPort = chain[random.randint(0, len(chain)-1)]
+    print("next SS is (\'{}\', {})".format(nextIP, nextPort))
+    chain.remove((nextIP, nextPort))
+
+    nextSSConn = socket.create_connection((nextIP, nextPort))
+    data = formatMessage(url, chain)
+    nextSSConn.send(data.encode())
+
+    print("waiting for file...")
+    response = nextSSConn.recv(1024).decode()
+    
+    print(response)
+    encoding = response.splitlines()[0]
+    content = response.msg[len(encoding):]
+    filename = url.split('/')[-1]
+    if filename == '' or '/' not in url:
+        filename = 'index.html'
+    with open(filename, 'w') as file:
+        file.write(content)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str)
+    parser.add_argument('-c', '--chainfile', type=str, default='chaingang.txt')
+    args = parser.parse_args()
+    run_awget(args)
+
+if __name__ == "__main__":
+    main()
--- ss.py
+++ ss.py
@@ -1,12 +1,83 @@
 ###############################################
-# Group Name  : XXXXXX
+# Group Name  : SAP
 
-# Member1 Name: XXXXXX
-# Member1 SIS ID: XXXXXX
-# Member1 Login ID: XXXXXX
+# Member1 Name: Ben Combs
+# Member1 SIS ID: 831850566
+# Member1 Login ID: bcombs18
 
 # Member2 Name: XXXXXX
 # Member2 SIS ID: XXXXXX
 # Member2 Login ID: XXXXXX
 ###############################################
 
+import sys
+import getopt
+import socket
+import threading
+import os
+import random
+import requests
+from typing import List
+
+
+def parseChain(chain):
+    chain = []
+    for line in chain.splitlines():
+        line = line.strip()
+        if line:
+            parsed = line.split()
+            ip = parsed[0]
+            port = int(parsed[1])
+            chain.append((ip, port))
+    return chain
+
+def runThread(clientConn):
+    data = clientConn.recv(1024)
+    url = data.splitlines()[0]
+    chain: List = data[len(url):]
+    if(len(chain) > 0):
+        try:
+            nextIP, nextPort = chain[random.randint(0, len(chain)-1)]
+            chain.remove((nextIP, nextPort))
+            nextSS = socket.create_connection((nextIP, nextPort))
+        except:
+            print("Failed to connect to next stepping stone")
+            exit(1)
+        clientConn.send(data.encode())
+    else:
+        encoding, text = requests.get(url, verify=False)
+        content = str(encoding) + "\n" + text
+        clientConn.send(content.encode())
+    clientConn.close()
+
+        
+def main():
+    portNumber = 8000
+    hostname = socket.gethostname()
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:", ["port"])
+    except getopt.GetoptError as err:
+        print(err)
+        print("Usage: ss.py [-p] <port number>")
+        exit(2)
+    for o, a in opts:
+        if o == "-p":
+            portNumber = a
+
+    print("ss: {}, {}".format(hostname, portNumber))
+
+    ssSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssSocket.bind(('', int(portNumber)))
+    ssSocket.listen(5)
+    connections = []
+    while True:
+        clientConnection, addr = ssSocket.accept()
+        try:            
+            currentThread = threading.Thread(target=runThread(clientConnection, ), daemon=True)
+            connections.append(currentThread)
+            currentThread.start()
+        except IOError:
+            exit(1)
+
+if __name__ == "__main__":
+    main()
